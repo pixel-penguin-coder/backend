993ac313152409d13302bbb2877894b9e5c76bcc
PixelPenguin <penguinpixel1234@gmail.com>
2023-08-03 22:06:29 +0800

    minor changes

---
+++ controllers/notes.js
@@ -0,0 +1,51 @@
+const notesRouter = require("express").Router();
+const Note = require("../models/note");
+require("express-async-errors");
+
+notesRouter.get("/", async (req, res) => {
+  const notes = await Note.find({});
+  res.json(notes);
+});
+
+notesRouter.get("/:id", async (req, res) => {
+  const note = await Note.findById(req.params.id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+notesRouter.post("/", async (req, res) => {
+  const body = req.body;
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  const savedNote = await note.save();
+  res.status(201).json(savedNote);
+});
+
+notesRouter.delete("/:id", async (req, res) => {
+  await Note.findByIdAndRemove(req.params.id);
+  res.status(204).end();
+});
+
+notesRouter.put("/:id", (req, res, next) => {
+  const body = req.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then((updatedNote) => {
+      res.json(updatedNote);
+    })
+    .catch((err) => next(err));
+});
+
+module.exports = notesRouter;
